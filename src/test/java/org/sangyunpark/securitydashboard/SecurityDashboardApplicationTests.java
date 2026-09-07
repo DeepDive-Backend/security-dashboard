840aff160c0002be120854907c176939c9bee44b
JustDoIt-99 <sangyunpark99@gmail.com>
2026-09-07 15:33:31 +0900

    chore: Spring 프로젝트 초기 설정

---
+++ src/test/java/org/sangyunpark/securitydashboard/SecurityDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sangyunpark.securitydashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
