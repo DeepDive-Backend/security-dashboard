6825f12e42eb238f6e76c9587b49f96914831b4a
JustDoIt-99 <sangyunpark99@gmail.com>
2026-09-12 12:57:58 +0900

    fix: Severity Enum import
    - Docker에서 jar 파일 실행시킬 때 컴파일 오류 발생

--- src/main/java/org/sangyunpark/securitydashboard/entity/SecurityEvent.java
+++ src/main/java/org/sangyunpark/securitydashboard/entity/SecurityEvent.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.print.attribute.standard.Severity;
 import java.time.LocalDateTime;
 
 @Entity
