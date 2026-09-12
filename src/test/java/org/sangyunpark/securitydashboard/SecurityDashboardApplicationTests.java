9566c4927ae72a2675f561fbf972a73f1b7e283c
JustDoIt-99 <sangyunpark99@gmail.com>
2026-09-12 13:35:16 +0900

    test: test Profile 적용

--- src/test/java/org/sangyunpark/securitydashboard/SecurityDashboardApplicationTests.java
+++ src/test/java/org/sangyunpark/securitydashboard/SecurityDashboardApplicationTests.java
@@ -2,8 +2,10 @@ package org.sangyunpark.securitydashboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SecurityDashboardApplicationTests {
 
     @Test
